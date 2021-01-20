ac77a9399a7ede2649fbcafc98ad2de911332e1c
shuchang <shuchang@agree.com.cn>
2021-01-20 20:49:27 +0800

    commit mindoc

---
+++ main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/astaxie/beego/session/memcache"
+	_ "github.com/astaxie/beego/session/mysql"
+	_ "github.com/astaxie/beego/session/redis"
+	"github.com/kardianos/service"
+	"github.com/lifei6671/mindoc/commands"
+	"github.com/lifei6671/mindoc/commands/daemon"
+	_ "github.com/lifei6671/mindoc/routers"
+	_ "github.com/mattn/go-sqlite3"
+)
+
+func main() {
+
+	if len(os.Args) >= 3 && os.Args[1] == "service" {
+		if os.Args[2] == "install" {
+			daemon.Install()
+		} else if os.Args[2] == "remove" {
+			daemon.Uninstall()
+		} else if os.Args[2] == "restart" {
+			daemon.Restart()
+		}
+	}
+	commands.RegisterCommand()
+
+	d := daemon.NewDaemon()
+
+	s, err := service.New(d, d.Config())
+
+	if err != nil {
+		fmt.Println("Create service error => ", err)
+		os.Exit(1)
+	}
+
+	if err := s.Run(); err != nil {
+		log.Fatal("启动程序失败 ->", err)
+	}
+}
